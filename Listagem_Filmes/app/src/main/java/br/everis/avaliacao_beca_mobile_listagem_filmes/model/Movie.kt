e955c1dafa3cc8e097d8097eb1abcce058419e89
AndreAliardi <andre.aliardi@gmail.com>
2021-03-30 10:53:48 -0300

    :construction: Criando um package para o Model e a data class Movie onde serão armazenados os dados de cada filme

---
+++ Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/model/Movie.kt
@@ -0,0 +1,40 @@
+package br.everis.avaliacao_beca_mobile_listagem_filmes.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+//    @SerializedName("page")
+//    val page: Int?,
+//    @SerializedName("results")
+//    val results: List<Objects>,
+    @SerializedName("adult")
+    val adult: Boolean?,
+    @SerializedName("backdrop_path")
+    val backdrop_path: String?,
+    @SerializedName("genre_ids")
+    val genre_ids: List<Int>?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("media_type")
+    val media_type: String?,
+    @SerializedName("original_language")
+    val original_language: String?,
+    @SerializedName("original_title")
+    val original_title: String?,
+    @SerializedName("overview")
+    val overview: String?,
+    @SerializedName("popularity")
+    val popularity: Float?,
+    @SerializedName("poster_path")
+    val poster_path: String?,
+    @SerializedName("release_date")
+    val release_date: String?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("video")
+    val video: Boolean?,
+    @SerializedName("vote_avarage")
+    val vote_avarage: String?,
+    @SerializedName("vote_count")
+    val vote_count: Int?
+)
