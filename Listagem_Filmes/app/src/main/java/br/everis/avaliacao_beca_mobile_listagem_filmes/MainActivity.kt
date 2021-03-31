8a0a43610e3b8da56e1754347f906c560106d0db
AndreAliardi <andre.aliardi@gmail.com>
2021-03-31 07:40:28 -0300

    :construction: instanciando e chamando o retrofit na MainActivity

--- Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MainActivity.kt
+++ Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MainActivity.kt
@@ -2,10 +2,20 @@ package br.everis.avaliacao_beca_mobile_listagem_filmes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerMovies: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerMovies = findViewById(R.id.recycler_movies)
+        recyclerMovies.layoutManager = LinearLayoutManager(this)
     }
+    
 }
