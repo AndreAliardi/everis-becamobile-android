8411e0a42b934659b2997575b9f38a5b9a6b6090
AndreAliardi <andre.aliardi@gmail.com>
2021-03-31 07:42:52 -0300

    :construction: criando método para trazer os dados do servidor com a implementação dos membros

--- Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MainActivity.kt
+++ Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import br.everis.avaliacao_beca_mobile_listagem_filmes.api.MyRetrofit
+import br.everis.avaliacao_beca_mobile_listagem_filmes.model.Movie
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +22,18 @@ class MainActivity : AppCompatActivity() {
         recyclerMovies = findViewById(R.id.recycler_movies)
         recyclerMovies.layoutManager = LinearLayoutManager(this)
     }
-    
+
+    private fun getData(){
+        val call: Call<List<Movie>> = MyRetrofit.instance?.movieApi()?.getMovieApi() as Call<List<Movie>>
+
+        call.enqueue(object : Callback<List<Movie>> {
+            override fun onFailure(call: Call<List<Movie>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call<List<Movie>>, response: Response<List<Movie>>) {
+                TODO("Not yet implemented")
+            }
+
+        }
 }
