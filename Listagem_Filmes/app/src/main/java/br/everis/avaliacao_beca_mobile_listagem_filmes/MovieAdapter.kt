9720328c665b00da31ddc4439d041eb47ff7f596
AndreAliardi <andre.aliardi@gmail.com>
2021-03-31 06:58:22 -0300

    :construction: Criando o Adapter com a implementação dos métodos

--- Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MovieAdapter.kt
+++ Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MovieAdapter.kt
@@ -1,10 +1,30 @@
 package br.everis.avaliacao_beca_mobile_listagem_filmes
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.everis.avaliacao_beca_mobile_listagem_filmes.model.Movie
 
+class MovieAdapter (
+    private val context: Context,
+    private val movieList: List<Movie>
+) : RecyclerView.Adapter<MovieViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
 
 class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val movie_poster: ImageView = itemView.findViewById(R.id.movie_poster)
