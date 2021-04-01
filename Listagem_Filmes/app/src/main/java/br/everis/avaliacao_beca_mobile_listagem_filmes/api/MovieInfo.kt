c5c0715e62ef0af168e1849a19c710c29126a232
AndreAliardi <andre.aliardi@gmail.com>
2021-03-31 22:51:36 -0300

    :construction: Criando uma data class que irá receber as informações dos filmes

---
+++ Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/api/MovieInfo.kt
@@ -0,0 +1,10 @@
+package br.everis.avaliacao_beca_mobile_listagem_filmes.api
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieInfo (
+    @SerializedName("poster_path") val poster_path: String,
+    @SerializedName("title") val title: String,
+//    @SerializedName("genre_ids") val genre_ids: List<String>,
+    @SerializedName("release_date") val release_date: String
+)
