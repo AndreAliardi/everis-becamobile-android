c0f02cc495dbe9e6b76d462d7dc51b850e4c4d1a
AndreAliardi <andre.aliardi@gmail.com>
2021-04-01 16:23:44 -0300

    :construction: criado a activity da segunda tela com as informações detalhadas dos filmes

---
+++ Listagem_Filmes/app/src/main/java/br/everis/avaliacao_beca_mobile_listagem_filmes/MovieDetail.kt
@@ -0,0 +1,11 @@
+package br.everis.avaliacao_beca_mobile_listagem_filmes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
